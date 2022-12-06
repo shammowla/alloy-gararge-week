e38a7b394e6a77168ddd739057d1790f324fc648
Carter McBride <18412686+carterworks@users.noreply.github.com>
2022-12-06 13:06:55 -0700

    Scaffold form and backend server
    Co-authored-by: Shamiul Mowla <shammowla@users.noreply.github.com>

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const port = 3001;
+app.use(express.static("build"));
+app.use(express.json());
+
+async function makeCustomBuild(configuration) {}
+
+app.get("/", (req, res) => {
+  res.redirect("/index.html");
+});
+
+app.post("/build", async (req, res) => {
+  const configuration = req.body;
+  const customBuild = await makeCustomBuild(configuration);
+  res.send("Done making custom build");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,162 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Formik, Field } from "formik";
+
+interface FormState {
+  orgId: string;
+  edgeConfigId: string;
+  includedComponents: [];
+}
 
 function App() {
+  const initialValues: FormState = {
+    orgId: "",
+    edgeConfigId: "",
+    includedComponents: [],
+  };
+  const onSubmit = async (values: FormState, { setSubmitting }: any) => {
+    const response = await fetch("/build", {
+      method: "POST",
+      body: values as any,
+    });
+
+    setSubmitting(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Alloy Garage Week: Alloy Custom Builds</h1>
       </header>
+      <main>
+        <Formik initialValues={{ ...initialValues }} onSubmit={onSubmit}>
+          <Form>
+            <div>
+              <label htmlFor="orgId">Organization ID</label>
+              <Field
+                type="text"
+                name="orgId"
+                id="orgId"
+                placeholder="ADB3LETTERSANDNUMBERS@AdobeOrg"
+              />
+            </div>
+            <div>
+              <label htmlFor="edgeConfigId">Edge Configuration ID</label>
+              <Field
+                type="text"
+                name="edgeConfigId"
+                id="edgeConfigId"
+                placeholder="ebebf826-a01f-4458-8cec-ef61de241c93"
+              />
+            </div>
+            <div id="checkbox-group">Included Components</div>
+            <ul role="group" aria-labelledby="checkbox-group">
+              <li>
+                <label>
+                  ActivityCollector
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="ActivityCollector"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  Audiences
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="Audiences"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  Context
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="Context"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  DataCollector
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="DataCollector"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  EventMerge
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="EventMerge"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  Identity
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="Identity"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  LibraryInfo
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="LibraryInfo"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  MachineLearning
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="MachineLearning"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  Personalization
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="Personalization"
+                  ></Field>
+                </label>
+              </li>
+              <li>
+                <label>
+                  Privacy
+                  <Field
+                    type="checkbox"
+                    name="includedComponents"
+                    value="Privacy"
+                  ></Field>
+                </label>
+              </li>
+            </ul>
+            <div>
+              <button type="submit">Build</button>
+            </div>
+          </Form>
+        </Formik>
+      </main>
     </div>
   );
 }
