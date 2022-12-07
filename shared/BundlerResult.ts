6787e253f60b692e0c98ca7afead334dd28a783d
Carter McBride <18412686+carterworks@users.noreply.github.com>
2022-12-07 10:27:25 -0700

    Show the server response in the UI

---
+++ shared/BundlerResult.ts
@@ -0,0 +1,5 @@
+interface BundlerResult {
+  success: boolean;
+}
+
+export default BundlerResult;
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Formik, Field } from "formik";
 import AlloyBuildConfig from "../shared/AlloyBuildConfig";
 
@@ -8,6 +8,7 @@ function App() {
     edgeConfigId: "",
     includedComponents: [],
   };
+  const [serverResponse, setServerResponse] = useState("{}");
   const onSubmit = async (values: AlloyBuildConfig, { setSubmitting }: any) => {
     const response = await fetch("/build", {
       method: "POST",
@@ -15,6 +16,9 @@ function App() {
     });
 
     setSubmitting(false);
+    // format the server response
+    const result = await response.json();
+    setServerResponse(JSON.stringify(result, null, 2));
   };
 
   return (
@@ -23,134 +27,149 @@ function App() {
         <h1>Alloy Garage Week: Alloy Custom Builds</h1>
       </header>
       <main>
-        <Formik initialValues={{ ...initialValues }} onSubmit={onSubmit}>
-          <Form>
-            <div>
-              <label htmlFor="orgId">Organization ID</label>
-              <Field
-                type="text"
-                name="orgId"
-                id="orgId"
-                placeholder="ADB3LETTERSANDNUMBERS@AdobeOrg"
-              />
-            </div>
-            <div>
-              <label htmlFor="edgeConfigId">Edge Configuration ID</label>
-              <Field
-                type="text"
-                name="edgeConfigId"
-                id="edgeConfigId"
-                placeholder="ebebf826-a01f-4458-8cec-ef61de241c93"
-              />
-            </div>
-            <div id="checkbox-group">Included Components</div>
-            <ul role="group" aria-labelledby="checkbox-group">
-              <li>
-                <label>
-                  ActivityCollector
+        <section>
+          <h2>Configure</h2>
+          <Formik initialValues={{ ...initialValues }} onSubmit={onSubmit}>
+            {({ isSubmitting }) => (
+              <Form>
+                <div>
+                  <label htmlFor="orgId">Organization ID</label>
                   <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="ActivityCollector"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  Audiences
+                    type="text"
+                    name="orgId"
+                    id="orgId"
+                    placeholder="ADB3LETTERSANDNUMBERS@AdobeOrg"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="edgeConfigId">Edge Configuration ID</label>
                   <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="Audiences"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  Context
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="Context"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  DataCollector
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="DataCollector"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  EventMerge
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="EventMerge"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  Identity
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="Identity"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  LibraryInfo
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="LibraryInfo"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  MachineLearning
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="MachineLearning"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  Personalization
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="Personalization"
-                  ></Field>
-                </label>
-              </li>
-              <li>
-                <label>
-                  Privacy
-                  <Field
-                    type="checkbox"
-                    name="includedComponents"
-                    value="Privacy"
-                  ></Field>
-                </label>
-              </li>
-            </ul>
-            <div>
-              <button type="submit">Build</button>
-            </div>
-          </Form>
-        </Formik>
+                    type="text"
+                    name="edgeConfigId"
+                    id="edgeConfigId"
+                    placeholder="ebebf826-a01f-4458-8cec-ef61de241c93"
+                  />
+                </div>
+                <div id="checkbox-group">Included Components</div>
+                <ul role="group" aria-labelledby="checkbox-group">
+                  <li>
+                    <label>
+                      ActivityCollector
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="ActivityCollector"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      Audiences
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="Audiences"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      Context
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="Context"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      DataCollector
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="DataCollector"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      EventMerge
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="EventMerge"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      Identity
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="Identity"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      LibraryInfo
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="LibraryInfo"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      MachineLearning
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="MachineLearning"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      Personalization
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="Personalization"
+                      ></Field>
+                    </label>
+                  </li>
+                  <li>
+                    <label>
+                      Privacy
+                      <Field
+                        type="checkbox"
+                        name="includedComponents"
+                        value="Privacy"
+                      ></Field>
+                    </label>
+                  </li>
+                </ul>
+                <div>
+                  <button type="submit" disabled={isSubmitting}>
+                    Build
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </section>
+        <section>
+          <h2>Result</h2>
+          <div>
+            <pre>
+              <code>{serverResponse}</code>
+            </pre>
+          </div>
+        </section>
       </main>
     </div>
   );
