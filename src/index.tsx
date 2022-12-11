529cc59e85c56db9f41ca3da88c1f14b42584d39
Shamiul Mowla <sham.mowla@gmail.com>
2022-12-11 15:55:05 -0500

    Add spectrum provider.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
+
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider theme={defaultTheme}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
